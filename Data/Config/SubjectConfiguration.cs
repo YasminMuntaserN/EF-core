e3c5180539cf60c52fddf06c06c81de35273b94f
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-27 11:43:48 +0200

    feat: add subject entity and configuration

---
+++ Data/Config/SubjectConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using study_center_ef.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_center_ef.Data.Config
+{
+    public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
+    {
+        public void Configure(EntityTypeBuilder<Subject> builder)
+        {
+            builder.HasKey(x => x.SubjectID);
+
+            builder.Property(x => x.SubjectName).HasColumnType("VARCHAR")
+                 .HasMaxLength(255).IsRequired();
+
+
+            builder.ToTable("Subjects");
+
+          /*  builder.HasMany(s => s.GradeLevels)
+                .WithMany(g => g.Subjects)
+                .UsingEntity<Dictionary<string, object>>(
+                    "GradeLevelSubjects",
+                    j => j.HasOne<GradeLevel>().WithMany().HasForeignKey("GradeLevelID"),
+                    j => j.HasOne<Subject>().WithMany().HasForeignKey("SubjectID")
+                );
+          */
+            builder.HasData(SeedData.LoadSubjects());
+            
+        }
+    }
+}
---
+++ Entities/Subject.cs
@@ -0,0 +1,11 @@
+﻿namespace study_center_ef.Entities
+{
+    public class Subject
+    {
+        public int SubjectID { get; set; }
+        public string? SubjectName { get; set; }
+       // public ICollection<GradeLevel> GradeLevels { get; set; } = new List<GradeLevel>();
+    }
+
+
+}
