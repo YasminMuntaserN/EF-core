6821a869d2174c0d71183e7a9919472646ebe392
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-27 11:41:44 +0200

    feat: add grade level subject entity and configuration

---
+++ Data/Config/GradeLevelSubjectConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using study_center_ef.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_center_ef.Data.Config
+{
+    public class GradeLevelSubjectConfiguration : IEntityTypeConfiguration<GradeLevelSubject>
+    {
+        public void Configure(EntityTypeBuilder<GradeLevelSubject> builder)
+        {
+            builder.HasKey(x => x.GradeLevelSubjectID);
+            builder.Property(x => x.GradeLevelSubjectID)
+            .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Title).HasColumnType("VARCHAR")
+                 .HasMaxLength(255).IsRequired();
+           
+            builder.Property(x => x.Fees).HasColumnType("int");
+
+            builder.ToTable("GradeLevelSubjects");
+
+        }
+    }
+}
---
+++ Entities/GradeLevelSubject.cs
@@ -0,0 +1,13 @@
+﻿namespace study_center_ef.Entities
+{
+    public class GradeLevelSubject
+    {
+        public int GradeLevelSubjectID { get; set; }
+        public int SubjectID { get; set; }
+        public int GradeLevelID { get; set; }
+        public string? Title { get; set; }
+        public int Fees { get; set; }
+        public Subject Subject { get; set; }
+        public GradeLevel GradeLevel { get; set; }
+    }
+}
