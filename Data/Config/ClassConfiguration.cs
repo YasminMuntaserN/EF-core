63d39c278c09c841817865f46a117665cdac4d35
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-27 11:41:05 +0200

    feat: add class entity and configuration

---
+++ Data/Config/ClassConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using study_center_ef.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_center_ef.Data.Config
+{
+    public class ClassConfiguration : IEntityTypeConfiguration<Class>
+    {
+        public void Configure(EntityTypeBuilder<Class> builder)
+        {
+            builder.HasKey(x => x.ClassID);
+
+            builder.Property(x => x.ClassName).HasColumnType("VARCHAR")
+                .HasMaxLength(50).IsRequired();
+
+            builder.Property(x => x.Description).HasColumnType("VARCHAR")
+                .HasMaxLength(50).IsRequired();
+
+
+            builder.Property(x => x.Capacity).HasColumnType("TINYINT").IsRequired();
+
+            builder.ToTable("Classes");
+            builder.HasData(SeedData.LoadClasses());
+        }
+    }
+}
