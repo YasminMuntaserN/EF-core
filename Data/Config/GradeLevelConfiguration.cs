f81420573a58c185c1cdc7c7fdf88266f0a2b2c0
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-27 11:41:24 +0200

    feat: add gradeLevel entity and configuration

---
+++ Data/Config/GradeLevelConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using study_center_ef.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_center_ef.Data.Config
+{
+    public class GradeLevelConfiguration : IEntityTypeConfiguration<GradeLevel>
+    {
+        public void Configure(EntityTypeBuilder<GradeLevel> builder)
+        {
+            builder.HasKey(x => x.GradeLevelID);
+
+            builder.Property(x => x.GradeLevelName).HasColumnType("VARCHAR")
+                 .HasMaxLength(255).IsRequired();
+
+
+            builder.ToTable("GradeLevels");
+            builder.HasData(SeedData.LoadGradeLevels());
+        }
+    }
+}
---
+++ Entities/GradeLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace study_center_ef.Entities
+{
+    public class GradeLevel
+    {
+        public int GradeLevelID { get; set; }
+        public string GradeLevelName { get; set; }
+      //  public ICollection<Subject> Subjects { get; set; } = new List<Subject>();
+    }
+
+
+}
