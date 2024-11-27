48ce11b21a3aa765910d2cb3204e06d2b70d8372
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-27 11:43:19 +0200

    feat: add student entity and configuration

---
+++ Data/Config/StudentConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using study_center_ef.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_center_ef.Data.Config
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(x => x.StudentID);
+            builder.Property(x => x.StudentID)
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.CreationDate).HasColumnType("DateTime")
+                .IsRequired();
+
+
+            builder.HasOne(x => x.Person)
+                   .WithOne(x => x.Student)
+                   .HasForeignKey<Student>(x => x.PersonID)
+                   .IsRequired();
+
+            builder.ToTable("Students");
+
+        }
+    }
+}
