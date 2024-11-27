eb6cfd9f10716dae645abba1ed8279fa3357b245
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-27 11:43:06 +0200

    feat: add person entity and configuration

---
+++ Entities/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace study_center_ef.Entities
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string LastName { get; set; }
+        public byte Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public Teacher Teacher { get; set; } 
+        public Student Student { get; set; } 
+
+
+    }
+
+
+}
