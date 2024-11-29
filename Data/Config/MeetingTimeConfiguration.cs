8d6c906a1accd06bc80925c4dc26ae8e70c76c22
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-29 20:39:44 +0200

    feat: add meetingTimes entity and configuration

---
+++ Data/Config/MeetingTimeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using study_center_ef.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_center_ef.Data.Config
+{
+    public class MeetingTimeConfiguration : IEntityTypeConfiguration<MeetingTime>
+    {
+        public void Configure(EntityTypeBuilder<MeetingTime> builder)
+        {
+            builder.HasKey(x => x.MeetingTimeID);
+
+            builder.Property(x => x.StartTime).HasColumnType("DateTime2")
+                .IsRequired();
+          
+            builder.Property(x => x.EndTime).HasColumnType("DateTime2")
+               .IsRequired();
+
+            builder.ToTable("MeetingTimes");
+
+            builder.HasData(SeedData.LoadMeetingTimes());
+        }
+    }
+}
