c6f6187595cc02ff80ab16de58bff5141ec24ec8
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-29 20:40:08 +0200

    feat: add payment entity and configuration

---
+++ Data/Config/PaymentConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using study_center_ef.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_center_ef.Data.Config
+{
+    public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+    {
+        public void Configure(EntityTypeBuilder<Payment> builder)
+        {
+            builder.HasKey(x => x.PaymentID);
+
+            builder.Property(x => x.Amount).HasColumnType("decimal").IsRequired();
+   
+            builder.Property(x => x.PaymentDate).HasColumnType("DateTime2")
+             .IsRequired();
+
+            // add one-many relationship between Student and Payments  
+            builder.HasOne(x => x.Student)
+                   .WithMany(x => x.Payments)
+                   .HasForeignKey(x => x.StudentID)
+                   .IsRequired();
+
+            // add one-many relationship between Group and Payments  
+            builder.HasOne(x => x.Group)
+                   .WithMany(x => x.Payments)
+                   .HasForeignKey(x => x.GroupID)
+                   .IsRequired();
+
+            builder.ToTable("Payments");
+
+
+            //builder.HasData(SeedData.LoadPayments());
+        }
+    }
+}
