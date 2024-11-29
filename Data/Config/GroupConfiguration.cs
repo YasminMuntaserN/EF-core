6d83de06a08922ccf76dbfa6d2116b352f10687c
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-29 20:40:40 +0200

    feat: add group entity and configuration

--- Data/Config/GroupConfiguration.cs
+++ Data/Config/GroupConfiguration.cs
@@ -15,26 +15,51 @@ namespace study_center_ef.Data.Config
         public void Configure(EntityTypeBuilder<Group> builder)
         {
             builder.HasKey(x => x.GroupID);
-            builder.Property(x => x.GroupID)
-             .ValueGeneratedOnAdd();
+            builder.Property(x => x.GroupID).ValueGeneratedOnAdd();
 
-            builder.Property(x => x.GroupName).HasColumnType("VARCHAR")
-                .HasMaxLength(255).IsRequired();
+            builder.Property(x => x.GroupName)
+                   .HasColumnType("VARCHAR")
+                   .HasMaxLength(255)
+                   .IsRequired();
 
-            builder.Property(x => x.CreationDate).HasColumnType("DateTime")
-                .IsRequired();
+            builder.Property(x => x.CreationDate)
+                   .HasColumnType("DateTime")
+                   .IsRequired();
 
-            builder.Property(x => x.GroupStudentCount).HasColumnType("int")
-                 .IsRequired();
+            builder.Property(x => x.GroupStudentCount)
+                   .HasColumnType("int")
+                   .IsRequired();
 
-            builder.Property(x => x.IsActive).HasColumnType("bit");
+            builder.Property(x => x.IsActive)
+                   .HasColumnType("bit");
 
-         /*   builder.HasOne(x => x.Class)
+
+            // one to many retationship between Class and Groups
+            builder.HasOne(x => x.Class)
                    .WithMany(x => x.Groups)
                    .HasForeignKey(x => x.ClassID)
-                   .IsRequired();*/
+                   .IsRequired();
+
+            // one to many retationship between GradeLevelSubject and Groups
+            builder.HasOne(x => x.GradeLevelSubject)
+                   .WithMany(x => x.Groups)
+                   .HasForeignKey(x => x.GradeLevelSubjectID)
+                   .IsRequired();
+
+            // one to many retationship between TeacherSubject and Groups
+            builder.HasOne(x => x.TeacherSubject)
+                   .WithMany(x => x.Groups)
+                   .HasForeignKey(x => x.TeacherSubjectID)
+                   .IsRequired();
+
+            // one to one retationship between MeetingTime and Group
+            builder.HasOne(x => x.MeetingTime)
+                   .WithOne(x => x.Group)
+                   .HasForeignKey<MeetingTime>(x => x.MeetingTimeID)
+                   .IsRequired();
 
             builder.ToTable("Groups");
+            builder.HasData(SeedData.LoadGroups());
         }
     }
 }
--- Entities/Group.cs
+++ Entities/Group.cs
@@ -10,16 +10,25 @@ namespace study_center_ef.Entities
     {
         public int GroupID { get; set; }
         public string GroupName { get; set; }
+       
         public int ClassID { get; set; }
+        public Class Class { get; set; }
+
         public int GradeLevelSubjectID { get; set; }
-        //public int TeacherSubjectID { get; set; }
-       // public int MeetingTimeID { get; set; }
+        public GradeLevelSubject GradeLevelSubject { get; set; }
+       
+        public int TeacherSubjectID { get; set; }
+        public TeacherSubject TeacherSubject { get; set; }
+
         public int GroupStudentCount { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreationDate { get; set; }
 
-        public Class Class { get; set; }
-        //  public TeacherSubject TeacherSubject { get; set; }
-        //  public MeetingTime MeetingTime { get; set; }
+         public int MeetingTimeID { get; set; }
+         public MeetingTime MeetingTime { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+        public ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
     }
 }
