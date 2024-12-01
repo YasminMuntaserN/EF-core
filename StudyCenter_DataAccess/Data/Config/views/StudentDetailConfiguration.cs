80b8d036f2e6bf9c87d457277c717f460409b43a
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-01 19:24:56 +0200

    feat: add delaing with view to create student detail

---
+++ StudyCenter_DataAccess/Data/Config/views/StudentDetailConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using study_center_ef.Entities.views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_center_ef.DataAccess.Data.Config.views
+{
+    public class StudentDetailConfiguration : IEntityTypeConfiguration<StudentDetail>
+    {
+        public void Configure(EntityTypeBuilder<StudentDetail> builder)
+        {
+            builder.HasNoKey();
+
+            builder.ToView("StudentsDetails");
+
+        }
+    }
+}
---
+++ StudyCenter_Entities/views/StudentsDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace study_center_ef.Entities.views
+{
+    public class StudentDetail
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string Gender { get; set; }
+        public string GradeLevel { get; set; }
+
+        public override string ToString()
+        => $"StudentID {StudentID} | TeacherName {StudentName} |Gender {Gender}  |GradeLevel {GradeLevel}";
+    }
+
+
+}
