78c44d2b032e8d4876abede5070e190fa99b2268
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-12-03 11:14:17 +0200

    feat : add validation exception

---
+++ StudyCenter_Exceptions/ValidationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_center_ef.StudyCenter_Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public IEnumerable<string> ValidationErrors { get; }
+
+        public ValidationException(IEnumerable<string> validationErrors)
+            : base("One or more validation errors occurred.")
+        {
+            ValidationErrors = validationErrors ?? throw new ArgumentNullException(nameof(validationErrors));
+        }
+    }
+}
